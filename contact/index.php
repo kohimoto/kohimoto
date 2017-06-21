b5578c6dc100c82f4919dac70222feb1974be819
kohimoto <kohimotoj@gmail.com>
2017-06-21 18:27:29 +0900

    ｃ

--- contact/index.php
+++ contact/index.php
@@ -1,7 +1,8 @@
 <?
 header("Content-Type: text/html;charset=Shift_JIS");
+include_once("../header.html");
 $data= file_get_contents('./form.html'); // シフトJISファイル読み込み
 //$str = mb_convert_encoding($data,"utf-8","sjis"); // シフトJISからUTF-8に変換
 echo $data; // 文字化けが起きない
-
+include_once("../footer.html");
 ?>
