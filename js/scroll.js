d98b07d10574cba8d42bf0ccf220da5b8141c981
kohimoto <kohimotoj@gmail.com>
2017-06-29 15:32:56 +0900

    contactスクロールごまかし

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
+  //color background
   $('.bg-c').spectrum();
   $(window).load(function(){
+    //scrollbar
     $('.description').mCustomScrollbar({
      advanced:{updateOnContentResize: true},
      theme: "dark-thin"
     });
+    //slider
     $('.flexslider').flexslider();
   });
 });
