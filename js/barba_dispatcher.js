292c75e375c6690145d6a1747fecb8e46b833f6d
kohimoto <kohimotoj@gmail.com>
2017-06-30 15:55:07 +0900

    いったんテストであげる。

--- js/barba_dispatcher.js
+++ js/barba_dispatcher.js
@@ -1,7 +1,9 @@
+$('body').removeClass('menu-show');
 Barba.Dispatcher.on("newPageReady", function(current, prev, container, raw){
     switch(current.namespace)
   {
     case "slider":
+alert("hey!!");
       //-----hover top--------//
       $('.works_d > img').hover(
         function(){
@@ -35,6 +37,31 @@ Barba.Dispatcher.on("newPageReady", function(current, prev, container, raw){
 
       //-----hover slider--------//
       $('.flexslider').flexslider();
+
+      //-----open nav--------//
+      //if menu_show is ,after moved slider remove.
+      $('body').removeClass('menu-show');
+
+      $('.js-fullheight').css('height', $(window).height());
+      $(window).resize(function(){
+      	$('.js-fullheight').css('height', $(window).height());
+      });
+      $('.js-fh5co-nav-toggle').on('click', function(event) {
+alert("start");
+        if( $('body').hasClass('menu-show') ) {
+alert("i have!");
+          $('body').removeClass('menu-show');
+alert("11");
+        } else {
+alert("don't have!");
+	  $('body').addClass('menu-show');
+alert("22");
+            setTimeout(function(){
+              $('#fh5co-main-nav > .js-fh5co-nav-toggle').addClass('show');
+            }, 900);
+        }
+      });
+
 
 
       break;
