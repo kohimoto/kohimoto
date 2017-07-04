4d371509ad27adaf942a0ae647371f942475f6d0
kohimoto <kohimotoj@gmail.com>
2017-07-04 12:30:06 +0900

    スライドの処理修正

--- js/load.js
+++ js/load.js
@@ -1,14 +1,14 @@
 $(function() {
   var h = $(window).height();
  
-  $('.wrap').css('display','none');
   $('#loader-bg ,#loader').height(h).css('display','block');
 });
  
 $(window).load(function () { //全ての読み込みが完了したら実行
-  $('#loader-bg').delay(900).fadeOut(800);
-  $('#loader').delay(600).fadeOut(300);
-  $('.wrap').css('display', 'block');
+  setTimeout(function(){
+    $('#loader-bg').delay(900).fadeOut(800);
+    $('#loader').delay(600).fadeOut(300);
+  },1000);
 });
  
 //10秒たったら強制的にロード画面を非表示
@@ -17,7 +17,6 @@ $(function(){
 });
  
 function stopload(){
-  $('.wrap').css('display','block');
   $('#loader-bg').delay(900).fadeOut(800);
   $('#loader').delay(600).fadeOut(300);
 }
