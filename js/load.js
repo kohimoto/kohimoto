920b9f90ed40cbfe2b60e5340b8dc8f2c4730977
kohimoto <kohimotoj@gmail.com>
2017-07-06 15:40:09 +0900

    いったんあげる。

--- js/load.js
+++ js/load.js
@@ -1,4 +1,8 @@
 $(function() {
+
+  $('.bg_top').css('display','none');
+  $('.list').css('display','none');
+  $('footer').css('display','none');
   var h = $(window).height();
  
   $('#loader-bg ,#loader').height(h).css('display','block');
@@ -9,6 +13,9 @@ $(window).load(function () { //全ての読み込みが完了したら実行
     $('#loader-bg').delay(900).fadeOut(800);
     $('#loader').delay(600).fadeOut(300);
   },1000);
+  $('.bg_top').css('display','block');
+  $('.list').css('display','block');
+  $('footer').css('display','block');
 });
  
 //10秒たったら強制的にロード画面を非表示
